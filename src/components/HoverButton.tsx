8e18a964c698d2274116f299de2811c3b0dacd13
Mike Hardy <github@mikehardy.net>
2022-03-10 16:15:49 -0500

    fix: fix text node issue rendering pressable

--- src/components/HoverButton.tsx
+++ src/components/HoverButton.tsx
@@ -16,22 +16,24 @@ export default function HoverButton(
   // const backgroundStyle = {backgroundColor: theme.colors.background, flex: 1};
 
   return (
-    <Pressable
-      style={styles.buttonContainer}
-      // @ts-ignore - because types don't have onHoverIn/onHoverOut - see react-native-web repo issues
-      onHoverIn={() => setHovered(true)}
-      onHoverOut={() => setHovered(false)}
-      // onPress={props.onPress}
-    >
-      <Button
-        mode={hovered || props.selected ? 'outlined' : 'text'}
-        onPress={() => {
-          console.log('hoverbutton: ' + props.buttonLabel);
-          props.onPress(props.buttonLabel);
-        }}>
-        {props.buttonLabel}
-      </Button>
-    </Pressable>
+    <>
+      <Pressable
+        style={styles.buttonContainer}
+        // @ts-ignore - because types don't have onHoverIn/onHoverOut - see react-native-web repo issues
+        onHoverIn={() => setHovered(true)}
+        onHoverOut={() => setHovered(false)}
+        // onPress={props.onPress}
+      >
+        <Button
+          mode={hovered || props.selected ? 'outlined' : 'text'}
+          onPress={() => {
+            console.log('hoverbutton: ' + props.buttonLabel);
+            props.onPress(props.buttonLabel);
+          }}>
+          {props.buttonLabel}
+        </Button>
+      </Pressable>
+    </>
   );
 }
 
