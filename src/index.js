be464e03914923a69a49a3b20885bd4ddd4d1fe1
Mike Hardy <github@mikehardy.net>
2022-03-10 16:16:10 -0500

    chore: turn off web vitals for performance at the moment
    we weren't using them (yet), better to disable

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 // RNVI integration for web
 import './icons';
@@ -18,4 +18,4 @@ AppRegistry.runApplication(appName, {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
